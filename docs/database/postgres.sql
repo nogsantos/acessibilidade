9b0a9d683056db912929d5a5d62de0f837bf2dd8
nogsantos <nogsantos@gmail.com>
2013-08-29 23:59:38 -0300

    Implementação de novas tabelas

--- docs/database/postgres.sql
+++ docs/database/postgres.sql
@@ -19,19 +19,24 @@ LANGUAGE 'sql' VOLATILE;
  */
 create schema pessoa;
 create table pessoa.pessoa(
-    id_pessoa bigint not null primary key  
+    id_pessoa bigint not null primary key,
+    
 );
+comment on table pessoa.pessoa is 'Tabela Pessoa é a especialização de pessoas no sistema.';
+comment on column pessoa.pessoa.id_pessoa is 'Chave principal de identificação da pessoa no sistema, pode ser um cpf(fisica) cnpj(juridica)';
 create table pessoa.fisica(
     nr_cpf bigint not null primary key,
     nm_pessoa varchar(250),
     foreign key (nr_cpf) references pessoa.pessoa(id_pessoa)
 );
+comment on table pessoa.fisica is 'Generalização de pessoa identificada pelo cpf.';
 create table pessoa.juridica(
     nr_cnpj bigint not null primary key,
     nm_razao_social varchar(250),
     nm_fantasia varchar(250),
     foreign key (nr_cnpj) references pessoa.pessoa(id_pessoa)
 );
+comment on table pessoa.juridica is 'Generalização de pessoa identificada pelo cnpj.';
 create table pessoa.usuario(
     fk_pessoa bigint not null primary key,
     login character varying(50) unique NOT NULL,
@@ -40,18 +45,20 @@ create table pessoa.usuario(
     data_bloqueio timestamp with time zone,
     foreign key (fk_pessoa) references pessoa.pessoa(id_pessoa)
 );
+comment on table pessoa.usuario is 'Generalização de pessoa, contém informações de acesso ao sistema.';
 create table pessoa.perfil(
-        id_perfil bigserial not null primary key,
-        nm_perfil varchar(250) unique,
-        desc_perfil varchar(500)
+    id_perfil bigserial not null primary key,
+    nm_perfil varchar(250) unique,
+    desc_perfil varchar(500)
 );
+comment on table pessoa.perfil is 'Definição do perfil dos usuários no sistema.';
 create table pessoa.perfil_usuario(
-	fk_perfil bigint not null,
-	fk_usuario bigint not null,
-	foreign key (fk_perfil) references pessoa.perfil(id_perfil),
-	foreign key (fk_usuario) references pessoa.pessoa(id_pessoa)
+    fk_perfil bigint not null,
+    fk_usuario bigint not null,
+    foreign key (fk_perfil) references pessoa.perfil(id_perfil),
+    foreign key (fk_usuario) references pessoa.pessoa(id_pessoa)
 );
-
+comment on table pessoa.perfil_usuario is 'Tabela intermediária entre perfil e usuário, permitindo ao usuário exercer mais de um papel no sistema.';
 insert into pessoa.pessoa values(99999999999);
 insert into pessoa.fisica values(99999999999, 'Administrador do Sistema');
 insert into pessoa.usuario values(99999999999,'admin','d033e22ae348aeb5660fc2140aec35850c4da997',now());
@@ -88,22 +95,25 @@ create table administrativo.modulo(
 	desc_modulo varchar(500),
 	ordem integer
 );
+comment on table administrativo.modulo is 'Módulos disponíveis no sistema.';
 create table administrativo.controller(
-	id_controller bigserial not null primary key,
-	fk_modulo bigint not null,
-	codigo_controller varchar(60) not null unique,
-	nm_controller varchar(250) not null unique,	
-	desc_controller varchar(500),
-	ordem integer,
-	foreign key (fk_modulo) references administrativo.modulo (id_modulo)	
+    id_controller bigserial not null primary key,
+    fk_modulo bigint not null,
+    codigo_controller varchar(60) not null unique,
+    nm_controller varchar(250) not null unique,	
+    desc_controller varchar(500),
+    ordem integer,
+    foreign key (fk_modulo) references administrativo.modulo (id_modulo)	
 );
+comment on table administrativo.controller is 'Controladores do sistema.';
 create table administrativo.action(
-	fk_controller varchar(60) not null,
-	codigo_action varchar(60) not null,
-	nm_action varchar(250) not null,	
-	desc_action varchar(500),	
-	foreign key (fk_controller) references administrativo.controller (codigo_controller)
+    fk_controller varchar(60) not null,
+    codigo_action varchar(60) not null,
+    nm_action varchar(250) not null,	
+    nm_action varchar(500),	
+    foreign key (fk_controller) references administrativo.controller (codigo_controller)
 );
+comment on table administrativo.action is 'Ações do sistema, cadastrar, editar, excluir etc.';
 create table administrativo.user_session( 
     session_id varchar(32) NOT NULL primary key, 
     session_user_id bigint, 
@@ -111,26 +121,39 @@ create table administrativo.user_session(
     session_data text NOT NULL,
     foreign key (session_user_id) references pessoa.usuario (fk_pessoa)
 );
-
+comment on table administrativo.user_session is 'Armazenamento dos dados da sessão do usuário.';
 create table administrativo.empresa(
     id_empresa bigserial not null primary key,
     nm_empresa varchar(250) not null,
     razao_social varchar(500) not null,
     nr_cnpj bigint not null unique,
-    empresa_matriz boolean, -- Confirma se é a empresa matriz.
-    fk_matriz bigint, -- Auto relacionamento, caso não seja matriz com a matriz.
-    prioridade_exibicao integer not null default (0), -- Define a prioridade de exibição.
+    empresa_matriz boolean,
+    fk_matriz bigint,
+    prioridade_exibicao integer not null default (0), 
     data_cadastro timestamp with time zone not null default(now()),
     data_bloqueio timestamp with time zone,
     foreign key (fk_matriz) references administrativo.empresa (id_empresa)
 );
-
+comment on table administrativo.empresa is 'Cadastro de empresas no sistema.';
+comment on column administrativo.empresa.empresa_matriz is 'Confirma se é a empresa matriz(true) ou filial(false).';
+comment on column administrativo.empresa.fk_matriz is 'Caso seja uma filial, informa quem é a matriz.';
+comment on column administrativo.empresa.prioridade_exibicao is 'Define a prioridade de exibição para seleção no formulário de login do sistema.';
+create table administrativo.empresa_telefone(
+    fk_empresa bigint not null,
+    fk_telefone not null,
+    flag_principal boolean,
+    foreign key (fk_empresa) references administrativo.empresa (id_empresa),
+    foreign key (fk_telefone) references public.telefone (id_telefone)
+);
+comment on table administrativo.empresa_telefone is 'Possibilita o cadastro de n telefones por empresa.';
+comment on column administrativo.empresa_telefone.principal is 'Define dentre os telefones cadastrados para a empresa, qual será o principal.';
 create table administrativo.perfil_empresa(
     fk_perfil bigint not null,
     fk_empresa bigint not null,
     foreign key (fk_perfil) references pessoa.perfil (id_perfil),
     foreign key (fk_empresa) references administrativo.empresa (id_empresa)	
 );
+comment on table administrativo.perfil_empresa is 'Tabela intermediária que permite um perfil ter acesso a mais de uma empresa do sistema.';
 
 insert into administrativo.modulo(codigo_modulo,nm_modulo,desc_modulo,ordem) values ('administrativo','Administrativo','Cadastro administrativos do sistema.',0);
 
@@ -251,3 +274,37 @@ create view vw_login_usuario as
     join administrativo.perfil_empresa pe on pe.fk_perfil = pu.fk_perfil
     join administrativo.empresa e on e.id_empresa = pe.fk_empresa
     where u.data_bloqueio is null
+
+/**Public*/
+create table public.endereco(
+    id_endereco bigserial not null primary key,
+    nm_logradouro varchar(500), 
+    nm_bairro varchar(250),
+    nr_lote integer,
+    nr_quadra varchar(50), 
+    nr_cep varchar(17),
+    fk_municipio bigint,
+    foreign key (fk_municipio) references public.municipio(id_municipio)
+);
+comment on table public.endereco is 'Cadastro global de endereços para as entidades que necessitem dessa opção no sistema.';
+create table public.telefone(
+    id_telefone bigserial not null primary key,
+    codigo_pais integer,
+    nr_ddd integer not null,
+    nr_telefone bigint not null, 
+    nm_tipo varchar(30) not null,
+    nm_operadora varchar(40)
+);
+comment on table public.telefone is 'Cadastro global de telefones para as entidades que necessitem dessa opção no sistema.';
+comment on column public.telefone.desc_tipo is 'Informa o tipo de telefone, fixo, fax, celular, trabalho, residencial etc.';
+create table public.email(
+    id_email bigserial not null primary key,
+    nm_email varchar(250) not null unique,
+);
+comment on table public.email is 'Cadastro global de endereços de email para as entidades que necessitem dessa opção no sistema.';
+create table public.municipio(
+    id_municipio bigserial not null primary key, 
+    nm_municipio varchar(250) not null, 
+    sigla_uf char(2) not null,
+    flag_capital boolean default(false)
+);
